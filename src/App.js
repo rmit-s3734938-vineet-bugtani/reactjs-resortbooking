d8b782ca82824c40b192fc06eda2b81f686a3799
Vineet <s3734938@student.rmit.edu.au>
2020-02-20 20:15:01 +0530

    Setting up routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './views/Home';
+import Rooms from './views/Rooms';
+import SingleRoom from './views/SingleRoom';
+import Error from './views/Error';
+import { Route, Switch} from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
-  return <>Hello from app</>;
+  return <>
+  <Navbar/>
+  <Switch>
+  <Route exact path="/" component={ Home }></Route>
+  <Route exact path="/rooms/" component={ Rooms }></Route>
+  <Route exact path="/rooms/:id" component={ SingleRoom }></Route>
+  <Route component={ Error }></Route>
+  </Switch>
+  </>;
 }
 
 export default App;
