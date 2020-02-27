fcd34d7dfd4da24347104305705c2cf2332eac1c
Vineet <s3734938@student.rmit.edu.au>
2020-02-27 12:43:25 +0530

    Room component implemented

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import defaultImg from "../images/room-1.jpeg";
+import { Link } from "react-router-dom";
 
-export default function Room() {
-    return (
-        <div>
-            Room Component
+export default function Room({ room }) {
+  const { name, slug, images, price } = room;
+  return (
+    <article className="room">
+      <div className="img-container">
+        <img src={images[0] || defaultImg} alt="Single room" />
+        <div className="price-top">
+          <h6>${price}</h6>
+          <p>per night</p>
         </div>
-    )
+        <Link to={`/rooms/${slug}`} className="btn-primary room-link">
+            Features
+          </Link>
+      </div>
+    </article>
+  );
 }
